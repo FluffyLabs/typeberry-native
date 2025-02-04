a930022c7f43066a55de8ceaaaecfcdd715de9ff
Mateusz Sikora <ms1qaz@gmail.com>
2025-02-04 15:25:09 +0100

    add pkg

---
+++ pkg/bandersnatch_wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @returns {boolean}
+*/
+export function verify_safrole(): boolean;
+/**
+* @param {Uint8Array} keys
+* @returns {Uint8Array}
+*/
+export function ring_commitment(keys: Uint8Array): Uint8Array;
