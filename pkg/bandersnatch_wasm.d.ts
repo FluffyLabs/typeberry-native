97749c611460ef704f75d7f9f1b264015a7ed4d4
Tomasz Drwięga <tomusdrw@gmail.com>
2025-03-31 22:22:31 +0200

    rebuild

--- pkg/bandersnatch_wasm.d.ts
+++ pkg/bandersnatch_wasm.d.ts
@@ -10,9 +10,25 @@ export function verify_safrole(): boolean;
 */
 export function ring_commitment(keys: Uint8Array): Uint8Array;
 /**
+* Seal verification as defined in:
+* https://graypaper.fluffylabs.dev/#/68eaa1f/0eff000eff00?v=0.6.4
+* or
+* https://graypaper.fluffylabs.dev/#/68eaa1f/0e54010e5401?v=0.6.4
+* @param {Uint8Array} keys
+* @param {number} signer_key_index
+* @param {Uint8Array} signature
+* @param {Uint8Array} seal_data
+* @param {Uint8Array} aux_data
+*/
+export function verify_seal(keys: Uint8Array, signer_key_index: number, signature: Uint8Array, seal_data: Uint8Array, aux_data: Uint8Array): void;
+/**
+* Verify multiple tickets at once as defined in:
+* https://graypaper.fluffylabs.dev/#/68eaa1f/0f3e000f3e00?v=0.6.4
+*
+* NOTE: the aux_data of VRF function is empty!
 * @param {Uint8Array} keys
 * @param {Uint8Array} tickets_data
-* @param {number} context_length
+* @param {number} vrf_input_data_len
 * @returns {Uint8Array}
 */
-export function verify_ticket(keys: Uint8Array, tickets_data: Uint8Array, context_length: number): Uint8Array;
+export function batch_verify_tickets(keys: Uint8Array, tickets_data: Uint8Array, vrf_input_data_len: number): Uint8Array;
--- pkg/bandersnatch_wasm_bg.wasm.d.ts
+++ pkg/bandersnatch_wasm_bg.wasm.d.ts
@@ -3,7 +3,8 @@
 export const memory: WebAssembly.Memory;
 export function verify_safrole(): number;
 export function ring_commitment(a: number, b: number, c: number): void;
-export function verify_ticket(a: number, b: number, c: number, d: number, e: number, f: number): void;
+export function verify_seal(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number): void;
+export function batch_verify_tickets(a: number, b: number, c: number, d: number, e: number, f: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_free(a: number, b: number, c: number): void;
