b29cbf6662168b37a0373ac56d91b914e59eac4c
Tomasz Drwięga <tomusdrw@gmail.com>
2025-09-09 21:54:03 +0200

    Bundle d.ts as well.

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,10 +1,13 @@
 import wasm from '@rollup/plugin-wasm';
+import { dts } from 'rolldown-plugin-dts';
 import { defineConfig } from 'rolldown';
 
 
 export default defineConfig({
   input: 'native/index.ts',
-  plugins: [wasm()],
+  plugins: [wasm(), dts({
+    resolve: true, // bundle all dependencies
+  })],
   output: {
     dir: 'dist',
   },
