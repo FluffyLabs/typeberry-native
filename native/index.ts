52b20cda10cc7156ef02e4e9e6840725002027f6
Tomek Drwięga <tomusdrw@users.noreply.github.com>
2025-09-10 12:46:28 +0200

    Bundle d.ts as well. (#16)
    * Bundle d.ts as well.
    
    * Try to build ed25519 for web?
    
    * Inline modules.
    
    * Hacks...
    
    * Manually initialize the modules.
    
    * Format code.

--- native/index.ts
+++ native/index.ts
@@ -2,3 +2,15 @@ export * as bandersnatch from "@typeberry/bandersnatch";
 export * as ed25519 from "@typeberry/ed25519";
 export * as reedSolomon from "@typeberry/reed-solomon";
 
+import bandersnatchInit from "@typeberry/bandersnatch";
+import bandersnatchWasm from "../bandersnatch/pkg/bandersnatch_bg.wasm";
+import ed25519Init from "@typeberry/ed25519";
+import ed25519Wasm from "../ed25519/pkg/ed25519_wasm_bg.wasm";
+import reedSolomonInit from "@typeberry/reed-solomon";
+import reedSolomonWasm from "../reed-solomon/pkg/reed_solomon_wasm_bg.wasm";
+
+export const wasm = {
+  bandersnatch: await bandersnatchInit(await bandersnatchWasm()),
+  ed25519: await ed25519Init(await ed25519Wasm()),
+  reedSolomon: await reedSolomonInit(await reedSolomonWasm()),
+};
--- rolldown.config.js
+++ rolldown.config.js
@@ -1,10 +1,15 @@
 import wasm from '@rollup/plugin-wasm';
+import { dts } from 'rolldown-plugin-dts';
 import { defineConfig } from 'rolldown';
 
 
 export default defineConfig({
   input: 'native/index.ts',
-  plugins: [wasm()],
+  plugins: [wasm({
+    maxFileSize: 100000000
+  }), dts({
+    resolve: true, // bundle all dependencies
+  })],
   output: {
     dir: 'dist',
   },
